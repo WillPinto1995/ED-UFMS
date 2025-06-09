b6a6d4acee2a50d552ba412fe0f6a304f457e9e7
WillPinto1995 <will_pinto@ufms.br>
2025-06-08 21:53:36 -0400

    Update kdtree_wrapper.py

--- kdtree_wrapper.py
+++ kdtree_wrapper.py
@@ -1,56 +1,83 @@
 import ctypes
-from ctypes import Structure, POINTER, c_float, c_char, c_int, c_double
+from ctypes import Structure, POINTER, c_float, c_char, c_int, c_void_p
 
-# =========================================
-# Estruturas compatíveis com a biblioteca C
-# =========================================
-
-class Registro(Structure):
+class FaceReg(Structure):
     _fields_ = [
         ("embedding", c_float * 128),
-        ("nome", c_char * 100)
+        ("id", c_char * 100)
     ]
 
-class NoKD(Structure):
+class TreeNode(Structure):
     pass
 
-NoKD._fields_ = [
-    ("key", ctypes.c_void_p),
-    ("esq", POINTER(NoKD)),
-    ("dir", POINTER(NoKD))
+TreeNode._fields_ = [
+    ("key", c_void_p),
+    ("esq", POINTER(TreeNode)),
+    ("dir", POINTER(TreeNode))
 ]
 
-class ArvoreKD(Structure):
+class KDTree(Structure):
     _fields_ = [
         ("k", c_int),
-        ("dist", ctypes.CFUNCTYPE(c_double, ctypes.c_void_p, ctypes.c_void_p)),
-        ("cmp", ctypes.CFUNCTYPE(c_int, ctypes.c_void_p, ctypes.c_void_p, c_int)),
-        ("raiz", POINTER(NoKD))
+        ("dist", c_void_p),
+        ("cmp", c_void_p),
+        ("raiz", POINTER(TreeNode))
     ]
 
-# =========================================
-# Carregando a biblioteca C
-# =========================================
+class KDTreeWrapper:
+    def __init__(self):
+        self.lib = ctypes.CDLL("/mnt/v/facom/ed/trabalho/tabalho_ed/api/libkdtree.so")
+        self._setup_functions()
 
-kdtree = ctypes.CDLL("./libkdtree.so")
+    def _setup_functions(self):
+        # Configuração dos tipos de argumentos e retorno
+        self.lib.kdtree_construir.argtypes = []
+        self.lib.kdtree_construir.restype = None
+        
+        self.lib.inserir_ponto.argtypes = [FaceReg]
+        self.lib.inserir_ponto.restype = None
+        
+        self.lib.buscar_n_vizinhos.argtypes = [
+            POINTER(KDTree),
+            POINTER(FaceReg),
+            c_int,
+            POINTER(POINTER(FaceReg))
+        ]
+        self.lib.buscar_n_vizinhos.restype = None
+        
+        self.lib.get_tree.restype = POINTER(KDTree)
 
-# =========================================
-# Definindo as funções da biblioteca C
-# =========================================
+    def initialize(self):
+        """Inicializa a KDTree"""
+        self.lib.kdtree_construir()
 
-# Construção da árvore
-kdtree.kdtree_construir.argtypes = []
-kdtree.kdtree_construir.restype = None
+    def build_tree(self):
+        """Constrói a estrutura da árvore"""
+        self.lib.kdtree_construir()
 
-# Inserção de um registro
-kdtree.inserir_ponto.argtypes = [Registro]
-kdtree.inserir_ponto.restype = None
+    def insert_face(self, embedding: list, face_id: str):
+        """Insere uma face na árvore"""
+        if len(embedding) != 128:
+            raise ValueError("Embedding deve ter 128 dimensões")
+        
+        emb_array = (c_float * 128)(*embedding)
+        face_bytes = face_id.encode('utf-8')[:99]
+        face_reg = FaceReg(embedding=emb_array, id=face_bytes)
+        self.lib.inserir_ponto(face_reg)
 
-# Recupera ponteiro para árvore global
-kdtree.get_tree.restype = POINTER(ArvoreKD)
-
-# Busca de N vizinhos mais próximos
-kdtree.buscar_n_vizinhos_proximos.argtypes = [
-    POINTER(ArvoreKD), Registro, POINTER(Registro), c_int
-]
-kdtree.buscar_n_vizinhos_proximos.restype = None
+    def search_faces(self, embedding: list, n: int = 5):
+        """Busca as n faces mais similares"""
+        emb_array = (c_float * 128)(*embedding)
+        query = FaceReg(embedding=emb_array, id=b"")
+        
+        tree_ptr = self.lib.get_tree()
+        result_type = POINTER(FaceReg) * n
+        results = result_type()
+        
+        self.lib.buscar_n_vizinhos(tree_ptr, query, n, results)
+        
+        return [
+            (results[i].contents.id.decode('utf-8').rstrip('\x00'), 
+             1.0 - getattr(results[i].contents, 'distancia', 0))
+            for i in range(n) if results[i]
+        ]
